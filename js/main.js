0405414b46e3186d65c56680fd0680366ef09752
cfgwo <wengellen@gmail.com>
2014-11-10 14:36:19 -0800

    Controller setup for production

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
 /**
  * Created by eweng on 11/7/2014.
  */
-var app = angular.module('app', []);
+
+var app = angular.module("app", []);
+
+// [ METHOD : 1 ]
+//var MainController = function($scope){
+//    $scope.val = "test123";
+//};
+
+// [ METHOD : 2 ] - DEVELOPMENT
+app.controller("MainController", function($scope){
+    $scope.value = "test123";
+});
+
+// [ METHOD : 3] - PRODUCTION
+//angular.module("controllers", []).controller("MainController", function($scope){
+//    $scope.value = "test123";
+//});

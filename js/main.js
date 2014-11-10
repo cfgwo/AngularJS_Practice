84d3d594e35fb43daf546669d545713dd0e71328
cfgwo <wengellen@gmail.com>
2014-11-10 15:32:15 -0800

    Interpolatin And Controllers

--- js/main.js
+++ js/main.js
@@ -4,17 +4,16 @@
 
 var app = angular.module("app", []);
 
-// [ METHOD : 1 ]
-//var MainController = function($scope){
-//    $scope.val = "test123";
-//};
-
 // [ METHOD : 2 ] - DEVELOPMENT
 app.controller("MainController", function($scope){
-    $scope.value = "test123";
+    $scope.value = 1;
+    $scope.even = false;
+
+    $scope.inc = function(){
+        $scope.value += 1;
+        $scope.even = $scope.value % 2 == 0;
+    };
 });
 
-// [ METHOD : 3] - PRODUCTION
-//angular.module("controllers", []).controller("MainController", function($scope){
-//    $scope.value = "test123";
-//});
+
+

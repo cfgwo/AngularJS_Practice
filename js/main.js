45412e76d9a89c0c4053031e659f7e15ea65c34b
cfgwo <wengellen@gmail.com>
2014-11-10 16:59:35 -0800

    11-2 : Ng-include

--- js/main.js
+++ js/main.js
@@ -6,9 +6,6 @@ var app = angular.module("app", []);
 
 // [ METHOD : 2 ] - DEVELOPMENT
 app.controller("MainController", function($scope){
-    $scope.classVar = ["orange", "italic"];
-    $scope.enableOrange = false;
-    $scope.enableItalic = true;
 
 });
 

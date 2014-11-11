0c11f351e644c53c468ee904c49b20be46c9167c
cfgwo <wengellen@gmail.com>
2014-11-10 16:29:43 -0800

    Ng-Repeat

--- js/main.js
+++ js/main.js
@@ -6,13 +6,11 @@ var app = angular.module("app", []);
 
 // [ METHOD : 2 ] - DEVELOPMENT
 app.controller("MainController", function($scope){
-    $scope.value = 1;
-    $scope.even = false;
+    $scope.myarr = [1, 2, 3, 4, 5];
 
-    $scope.inc = function(){
-        $scope.value += 1;
-        $scope.even = $scope.value % 2 == 0;
-    };
+    $scope.users = [{name:"ellen", age:28},{name:"paul", age:28}];
+
+    $scope.ob = {name: "mike", age:35, title:"Mr."};
 });
 
 

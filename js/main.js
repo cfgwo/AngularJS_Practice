df961c04da01e551f8423cd17cdc89ae0bd50410
cfgwo <wengellen@gmail.com>
2014-11-10 21:13:45 -0800

    20 : Constants And Values

--- js/main.js
+++ js/main.js
@@ -5,13 +5,24 @@
 var app = angular.module("app", []);
 
 // [ METHOD : 2 ] - DEVELOPMENT
-app.controller("MainController", function($scope, $filter){
-    $scope.mydata = {arr: [{name:"jane", age:33}, {name:"jake", age:33},{name:"steven", age:49}, {name:"hank", age:44}]}
-    // you can also use filter in the console
-    console.log($filter('uppercase')('make this string uppercase!'));
+app.controller("MainController", function($scope, constService, valService){
+    console.log(constService.attr, valService());
 });
 
 
+app.constant('twitterAPI', {url: "api.twitter.com/v1/"});
+app.constant('constService', {attr: "this is const data!"});
+
+app.value('valService', function(){ return "this is the returned from a fn";});
 
 
+/*
+Service Types:
+Constants
+Values
+Factories
+Services
+Providers
+Decorators
 
+*/

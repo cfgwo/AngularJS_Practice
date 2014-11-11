6a1265fa7c4331ca503793ff13500f54f1d27620
cfgwo <wengellen@gmail.com>
2014-11-10 18:03:59 -0800

    16 : Built-In-Filters

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ var app = angular.module("app", []);
 
 // [ METHOD : 2 ] - DEVELOPMENT
 app.controller("MainController", function($scope){
-    $scope.mydata = "This is some data"
+    $scope.mydata = {arr: [{name:"jane", age:35}, {name:"jake", age:34},{name:"steven", age:44}, {name:"hank", age:44}]}
 });
 
 
